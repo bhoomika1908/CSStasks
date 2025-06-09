e0f7dc354b89e9429fd0933b79686d5bf192b845
bhoomika1908 <dinnebhoomika1908@gmail.com>
2025-06-09 21:54:56 +0530

    media

--- pyhon.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter a number"))
-if n==1 or n==0:
-    print("not a prime number")
-for i in range(2,n):
-    if n%i==0:
-        print("is not a prime")
-        break
-else:
-    print("is a prime")
